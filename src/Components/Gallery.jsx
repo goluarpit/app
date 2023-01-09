9b0e1c3fa04032cf3d9d60d94c0bbb9ef214e7ff
goluarpit <golubaap13@gmail.com>
2023-01-09 14:41:48 +0530

    app

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -5,8 +5,8 @@ export default class Gallery extends Component{
        <div>
         <h1 style={{color:"green"}}><center>Gallery</center></h1>
                     <div style={{paddingLeft:"100px"}}><img src='images/Wt3_qOtDOhS.jpeg' height="300px" width="400px"/>
-                    <img src='images/kid.jpg' height="300px" width="400px"/>
-                    <img src='images/kk4.jpeg' height="300px" width="400px"/>
+                    <img src='images/kid6' height="300px" width="400px"/>
+                    <img src='images/kk5.jpeg' height="300px" width="400px"/>
                     <img src='images/kk5.jpeg' height="300px" width="400px"/>
                     </div>
                     <div style={{paddingLeft:"100px"}}>
